0fd890d4e4de2fc6ceeda9d5e36d3df80bdde4f1
trangluong <trangluong2305@gmail.com>
2018-11-28 12:13:19 -0500

    fix bug of single digit hour and minute

--- run.py
+++ run.py
@@ -151,7 +151,7 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow): #update the GUI window 
         print("Dispay Re-translated")
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
-        self.label.setText(_translate("MainWindow", "Alarm currently Turned off", None))
+        self.label.setText(_translate("MainWindow", "Alarm is currently Turned off", None))
         self.pushButton.setText(_translate("MainWindow", "Set Alarm", None))
     
     def Time(self): #Function to compare current time with set time 
@@ -160,24 +160,28 @@ class Ui_MainWindow(object):
         self.current_m = int (strftime("%M"))
         if (self.current_h == self.alarm_h) and (self.current_m == self.alarm_m) and ((int(strftime("%S"))%15) == 0): #if the both time match 
             message1 = " The time is " + str(self.alarm_h) + " : " + str(self.alarm_m) + " on " + strftime("%A")
-            message = "Sir, Good morning.. This is your wake up Alarm." + message1
+            message2 = "Alarm is currently turned off"
             self.label.setText(_translate("MainWindow",message1, None)) #display the message on GUI screen  
 ##            espeak.synth (message) #speak the message through audio jack
                 # --- turn speakers on ---
             if 'linux' in platform:
                 os.system(app_directory + '/audio_output/./AUDIO_JACK.sh')
             main()
-##            time.sleep(5*random.random())
+            time.sleep(5*random.random())
+            self.label.setText(_translate("MainWindow",message2, None))
              # --- turn speakers off ---
             if 'linux' in platform:
                 os.system(app_directory + '/audio_output/./HDMI_out.sh')
             
     def button_pressed(self): #when set alarm button is pressed 
         print("Button Pressed")
+        alarm_time = self.Set_Time.time()
+        alarm_time_hour = alarm_time.hour()
+        alarm_time_minute = alarm_time.minute()
         alarm_time = str(self.Set_Time.time())
         
-        self.alarm_h = int(alarm_time[19:21]) #value of hour is sotred in index value 19 and 20
-        self.alarm_m = int (alarm_time[23:25]) #value of minute is sotred in index value 23 and 24
+        self.alarm_h = int(alarm_time_hour)
+        self.alarm_m = int (alarm_time_minute) 
         message = "Alarm is set at " + str(self.alarm_h) + " hours " + str(self.alarm_m) + " minutes"
         self.label.setText(_translate("MainWindow", message, None)) #display the message on GUI screen  
 ##        espeak.synth (message) #speak the message through audio jack 
